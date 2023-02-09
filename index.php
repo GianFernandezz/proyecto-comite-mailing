de73f5624bff97a7b4964a1e68b4aed8b0a9e323
Giancarlo Fernandez <markcarranzafer12@gmail.com>
2023-02-09 12:00:36 -0500

    Proyecto landing comite SST mailing

---
+++ index.php
@@ -0,0 +1,390 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- 
+    - primary meta tags
+  -->
+  <title>ItePrevengo - Comité SST</title>
+  <meta name="title" content="Evita sanciones por no contar con un Comité SST">
+  <meta name="description" content="Entrenamiento + Consultoría Online + Software = CERO SANCIONES">
+
+  <!-- 
+    - favicon
+  -->
+ <link rel="icon" type="image/x-icon" href="./assets/images/iteprevengo.jfif">
+
+  <!-- 
+    - google font link
+  -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap" rel="stylesheet">
+
+  <!-- 
+    - custom css link
+  -->
+  <link rel="stylesheet" href="./assets/css/style.css">
+
+
+</head>
+
+<body>
+      
+
+      
+  <main>
+    <article>
+      <!-- 
+        - #SERVICE
+      -->
+
+      <section class="section service">
+        <div class="container">
+          <h1 class="h1 section-title">
+            ¿Quieres <strong class="resaltar">evitar sanciones</strong>  <strong class="resaltar">por no contar</strong> con un <strong class="resaltar">comité</strong> o <strong class="resaltar">supervisor</strong> de seguridad y salud?
+          </h1>
+          <p class="section-subtitle">Aún sin ser un especialista y sin necesidad de un consultor en tu empresa</p>
+        </div>
+      </section>
+
+      <!-- 
+        - #ABOUT
+      -->
+
+      <section class="about" aria-labelledby="about-label">
+        <div class="container">
+
+          <div class="hero-card">
+
+            <a href="./presentacion.php">
+              <figure class="about-banner">
+                <img src="./assets/images/portada-video.jpg" width="800" height="580" loading="lazy" alt="video-img"
+                  class="w-100">
+              </figure>
+    
+              <button class="play-btn" aria-label="play adex intro">
+                <ion-icon name="play" aria-hidden="true"></ion-icon>
+                      
+              </button>
+            </a>
+
+          </div>
+
+          <div class="about-content">
+
+            <h2 class="h2 section-title">
+              En esta presentación te explicamos:
+            </h2>
+
+            <ul>
+
+              <li class="about-item">
+                  <h3 class="card-title">
+                    <a href="./presentacion.php">
+                      <button class="accordion-btn">
+                        <ion-icon name="checkmark-outline"></ion-icon>
+
+                        <p class="span h3 btn-abrir">Los pasos para lograr la conformación del comité o supervisor SST</p>
+                      </button>
+                    </a>
+                  </h3>
+              </li>
+              <li class="about-item">
+                  <h3 class="card-title">
+                    <a href="./presentacion.php">
+                      <button class="accordion-btn">
+                        <ion-icon name="checkmark-outline"></ion-icon>
+
+                        <p class="span h3 btn-abrir">Nuestras alternativas para lograrlo</p>
+                      </button>
+                    </a>
+                  </h3>
+              </li>
+
+              <li class="about-item">
+                  <h3 class="card-title">
+                    <a href="./presentacion.php">
+                      <button class="accordion-btn">
+                        <ion-icon name="checkmark-outline"></ion-icon>
+
+                        <p class="span h3">Software para el proceso de elecciones</p>
+                      </button>
+                    </a>
+                  </h3>
+              </li>
+
+              <!-- BOTON -->
+
+              <button>
+                <a href="./presentacion.php" class="btn btn-primary">Ver Presentación</a> 
+              </button>             
+
+            </ul>
+
+          </div>
+
+        </div>
+      </section>
+
+      <!-- 
+        - #QUIENES SOMOS
+      -->
+
+      <section class="section feature" aria-labelledby="feature-label">
+        <div class="container">
+
+          <figure class="feature-banner">
+            <a href="https://www.linkedin.com/in/herbert-parimango/?originalSubdomain=pe" target="_blank">
+            <img src="./assets/images/herbert-nuevo.jpg" width="800" height="531" loading="lazy" alt="Herbert"
+              class="w-100">
+            </a>
+            
+          </figure>
+
+          <div class="feature-content">
+
+            <a href="https://www.linkedin.com/in/herbert-parimango/?originalSubdomain=pe" target="_blank">
+              <p class="section-subtitle nosotros">Acerca de nosotros</p>
+
+              <h2 class="h2 section-title">¿Quiénes somos?</h2>
+
+              <p class="section-text">
+                Iteprevengo nace en el 2013 con el nombre de P&M HSEQ, de la mano de su fundador y CEO Ing. Herbert Parimango y un equipo de especialistas en prevención de accidentes y enfermedades ocupacionales, con el objetivo de hacer de los centros de trabajo, espacios 100% libres de accidentes. <br> 
+                Hoy, Iteprevengo renueva su compromiso de brindar servicios de soporte para la prevención de accidentes y enfermedades en el trabajo a través de la transformación digital de sus servicios y productos como Software para la gestión de seguridad y salud, Capacitaciones Virtuales, Online y presenciales , Consultorías online y semi presenciales y monitoreos de higiene ocupacional.
+              </p>
+            </a>
+
+          </div>
+
+        </div>
+      </section>
+
+      <!-- UN SECTION PARA LOS CLIENTES _ MARCAS -->
+      <section class="container cliente">
+        <!-- cliente -->
+
+          <h2 class="h2 cliente-titulo">Nuestros Clientes</h2>
+
+          <div class="carrusel">
+              <div class="carrusel-items">
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/abb.jpg" alt="abb" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/british-american-tobacco.jpg" alt="british-american-tobacco" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/claro.jpg" alt="claro" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/confitrol.jpg" alt="confitrol" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/fisac.jpg" alt="fisac" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/gerdau.jpg" alt="gerdau" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/grupoefe.jpg" alt="grupoefe" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/hidrandina.jpg" alt="hidrandina" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/hochschild.jpg" alt="hochschild" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/liderman.jpg" alt="liderman" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/nexa.jpg" alt="nexa" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/pacasmayo.jpg" alt="pacasmayo" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/pacifico.jpg" alt="pacifico" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/petroperu.jpg" alt="petroperu" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/preciouno.jpg" alt="preciouno" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/quikrete.jpg" alt="quikrete" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/reyser.jpg" alt="reyser" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/saeg.jpg" alt="saeg" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/siderperu.jpg" alt="siderperu" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/skf.jpg" alt="skf" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/ssk.jpg" alt="ssk" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/tottus.jpg" alt="tottus" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/villa-salud.jpg" alt="villa-salud" />
+                  </div>
+                  <div class="carrusel-item">
+                      <img src="./assets/images/clientes/willis.jpg" alt="willis" />
+                  </div>
+                  
+                  
+              </div>
+          </div>
+
+          <!-- cliente fin -->
+
+      </section>
+
+    </article>
+  </main>
+
+
+
+
+
+  <!-- 
+    - #FOOTER
+  -->
+
+  <footer class="footer">
+    <div class="container grid-list">
+
+      <div class="footer-brand">
+
+        <a href="https://www.linkedin.com/company/iteprevengo/" class="logo" target="_blank">
+          <img src="./assets/images/logo-iteprevengo.png" width="300" height="24" alt="ItePrevengo">
+        </a>
+
+        <p class="footer-text">
+          &copy; 2023 ItePrevengo. <br> All rights reserved.
+        </p>
+
+        <ul class="social-list">
+
+          <li>
+            <a href="https://www.linkedin.com/company/iteprevengo/" class="social-link" target="_blank">
+              <ion-icon name="logo-linkedin"></ion-icon>
+            </a>
+          </li>
+
+          <li>
+            <a href="https://www.facebook.com/ItePrevengo" class="social-link" target="_blank">
+              <ion-icon name="logo-facebook"></ion-icon>
+            </a>
+          </li>
+
+          <li>
+            <a href="https://www.iteprevengo.com/" class="social-link" target="_blank">
+              <ion-icon name="logo-dribbble"></ion-icon>
+            </a>
+          </li>
+
+          <li>
+            <a href="https://wa.me/51943776124" class="social-link" target="_blank">
+              <ion-icon name="logo-whatsapp"></ion-icon>
+            </a>
+          </li>
+
+          <li>
+            <a href="mailto:consultas@iteprevengo.com" class="social-link" target="_blank">
+              <ion-icon name="mail-outline"></ion-icon>
+            </a>
+          </li>
+
+        </ul>
+
+      </div>
+
+      <ul class="footer-list">
+
+        <li>
+          <p class="h4 footer-list-title">Hablemos</p>
+        </li>
+
+        <li>
+          <a href="mailto:consultas@iteprevengo.com" class="footer-link">consultas@iteprevengo.com</a>
+        </li>
+
+        <li>
+          <a href="tel:014800947" class="footer-link">
+            (01) 480 0947</a>
+        </li>
+        <li>
+          <a href="tel:+51943776124" class="footer-link">
+            +51 943 776 124</a>
+        </li>
+
+      </ul>
+
+      <ul class="footer-list">
+
+        <li>
+          <p class="h4 footer-list-title">Empresa</p>
+        </li>
+
+        <li>
+          <a href="https://www.iteprevengo.com/quienes-somos/" class="footer-link" target="_blank">Quiénes Somos</a>
+        </li>
+
+        <li>
+          <a href="https://www.iteprevengo.com/terminos-y-condiciones/" class="footer-link" target="_blank">Términos y Condiciones</a>
+        </li>
+
+        <li>
+          <a href="https://www.iteprevengo.com/politicas-de-privacidad/" class="footer-link" target="_blank">Politicas de Privacidad</a>
+        </li>
+      </ul>
+
+      <div class="footer-list">
+
+        <p class="h4 footer-list-title">Propósito</p>
+
+        <p class="footer-text">
+          Nuestro propósito es ayudarte a reducir a cero los accidentes y enfermedades producidas en el trabajo, usando herramientas digitales para sus servicios de capacitación, consultoría, supervisión, monitoreos ocupacionales e implementación de la gestión de Seguridad y Salud en el Trabajo
+        </p>
+
+        
+
+      </div>
+
+    </div>
+  </footer>
+
+
+
+
+
+  <!-- 
+    - custom js link
+  -->
+  <script src="./assets/js/main.js"></script>
+  
+
+  <!-- 
+    - ionicon
+  -->
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+</body>
+
+</html>
---
+++ presentacion.php
@@ -0,0 +1,385 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- 
+    - primary meta tags
+  -->
+  <title>ItePrevengo - Comité SST</title>
+  <meta name="title" content="Evita sanciones por no contar con un Comité SST">
+  <meta name="description" content="Entrenamiento + Consultoría Online + Software = CERO SANCIONES">
+
+  <!-- 
+    - favicon
+  -->
+ <link rel="icon" type="image/x-icon" href="./assets/images/iteprevengo.jfif">
+
+  <!-- 
+    - google font link
+  -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap" rel="stylesheet">
+
+  <!-- 
+    - custom css link
+  -->
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <link rel="stylesheet" href="./assets/css/presentacion.css">
+  <!-- Swiper -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+  
+
+</head>
+
+<body>
+
+      <!-- 
+        - #BOTON-FLOTANTE-WHATSAPP
+      -->
+      <a href="https://wa.me/51943776124" class="btn-wsp" target="_blank">
+        <ion-icon name="logo-whatsapp" class="whats"></ion-icon> 
+      </a>
+
+  <main>
+    <article>
+
+        <!-- 
+        - #SANCIONES
+        -->
+
+      <section class="section sanciones">
+        <div class="container">
+          <div class="sombrear">
+          <p class="h2 section-subtitle-presentacion">Entrenamiento + Consultoría + Software = CERO SANCIONES</p>
+          </div>
+        </div>
+      </section>
+
+
+        <!-- 
+        - #VIDEO - REPRODUCIR
+        -->
+
+        <section class="video">
+            
+                
+            <div class="video-main">
+              <iframe src="https://player.vimeo.com/video/797083479?h=4779914251" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+            </div>
+
+            
+        </section>
+
+        <center>
+          <button class="agendar-cita">
+            <a href="./contacto-cita.php" class="btn btn-primary" target="_blank">Agendar una cita</a>
+        </button>
+        </center>
+        <!-- 
+        - #IMG -
+        -->
+
+        <section class="imagenes-informativas">
+          <div class="container">
+            <div class="img-title">
+              <h3 class="h2 section-title">Nuestro Servicio Comité SST</h3>
+            </div>
+            <div class="img-info">  
+              <img src="./assets/images/img-1.jpg" alt="img-informativa">
+            </div>
+            <div class="img-title">
+              <h3 class="h2 section-title">Escala de Multas</h3>
+            </div>
+            <div class="img-info">
+                  <img src="./assets/images/escala-de-multas expresada-en-soles.jpg" alt="img-informativa">
+            </div>
+          </div>
+        </section>
+
+      <!-- 
+        - # Trayectoria
+      -->
+
+      <section class="section feature" aria-labelledby="feature-label">
+        <div class="container">
+
+          <figure class="feature-banner">
+            <a href="https://www.linkedin.com/in/herbert-parimango/?originalSubdomain=pe" target="_blank">
+              <img src="./assets/images/herbert-nuevo.jpg" width="800" height="531" loading="lazy" alt="Herbert"
+              class="w-100">
+            </a>
+          </figure>
+
+          <div class="feature-content">
+
+            <a href="https://www.linkedin.com/in/herbert-parimango/?originalSubdomain=pe" target="_blank">
+              <p class="section-subtitle nosotros">Acerca de Herbert Parimango</p>
+
+              <h2 class="h2 section-title">Trayectoria Profesional</h2>
+
+              <p class="section-text">
+                Ing. Industrial con más de 19 años de experiencia profesional en la Gestión de Seguridad y Salud en el Trabajo y Gestión de Actividades de Alto Riesgo, ha laborado en importantes empresas del sector Industrial, Construcción y proyectos en Minería. 
+                  <br>
+                Cuenta con estudios de especialización en Sistemas Integrados de Gestión, Transformación Digital, OSHA y TEEX.
+                  <br>
+                Fundador de Iteprevengo, CEPAR e ItePrevengo Software.
+
+              </p>
+            </a>
+
+          </div>
+
+        </div>
+      </section>
+
+      <!-- UN SECTION PARA COMENTARIOS CLIENTES-->
+        <section class="container">
+
+            <div id="page" class="site">
+                <div class="testimoniales"> <!-- cambiar el container-->
+                    <div class="testi">
+                        <div class="head">
+                            <h3 class="h2 section-title">Nuestros Clientes Opinan</h3>
+                            <p>Testimonios..</p>
+                        </div>
+                        <div class="body swiper">
+                            <ul class="swiper-wrapper">
+                                <li class="swiper-slide">
+                                    <div class="wrapper">
+                                        <div class="thumbnail">
+                                            <img src="./assets/images/cliente1.jpeg" alt="Marco Luna Placencia">
+                                        </div>
+                                        <div class="aside">
+                                            <p>Confiamos t recomendamos usar ItePrevengo Software. Porque gestionamos nuestro Plan Covid, obteniendo el reporte y seguimiento oportuno de los detectados. Con los módulos de: Inspección de SST, Entrega de EPP's, Notificación de documentos y Vacunación; logramos la participación de colaboradores y líderes, y la conservación y acceso de todos los registros.</p>
+                                            <div class="name">
+                                                <h4>Marco Luna Placencia</h4>
+                                                <p>Gerente de Administración de Personal y Nóminas - GRUPO EFE</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="swiper-slide">
+                                    <div class="wrapper">
+                                        <div class="thumbnail">
+                                            <img src="./assets/images/cliente2.png" alt="Jaime Mendoza">
+                                        </div>
+                                        <div class="aside">
+                                            <p>Iteprevengo Software nos permite gestionar eficientemente la sintomatología, seguimiento, altas y documentación de casos Covid-19, de más de 8 mil colaboradores de nuestros clientes, Asimismo integra módulos de seguridad en el trabajo. Recomiendo usar ItePrevengo Software.</p>
+                                            <div class="name">
+                                                <h4>Jaime Mendoza</h4>
+                                                <p>Especialista En Salud Ocupacional (CEO - SSMAT)</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="swiper-slide">
+                                    <div class="wrapper">
+                                        <div class="thumbnail">
+                                            <img src="./assets/images/cliente3.jpg" alt="Jose Moises Yupanqui">
+                                        </div>
+                                        <div class="aside">
+                                            <p>El módulo de elecciones de comité de seguridad y salud de ItePrevengo Software, permitió que nuestros colaboradores elijan a sus representantes de forma sencilla y segura con resultados inmediatos y documentación exigida por ley.</p>
+                                            <div class="name">
+                                                <h4>Jose Moises Yupanqui</h4>
+                                                <p>Jefe De Seguridad Y Salud Empresa Agroindustria Laredo S.A.A.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+        
+                            <div class="swiper-pagination"></div>
+        
+                            <!-- If we need navigation buttons -->
+                            <div class="swiper-button-prev"></div>
+                            <div class="swiper-button-next"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+
+        <center><button class="agendar-cita">
+            <a href="./contacto-cita.php" class="btn btn-primary" target="_blank">Agendar una cita</a>
+        </button></center>
+
+    </article>
+  </main>
+
+
+
+
+
+  <!-- 
+    - #FOOTER
+  -->
+
+  <footer class="footer">
+    <div class="container grid-list">
+
+      <div class="footer-brand">
+
+        <a href="https://www.linkedin.com/company/iteprevengo/" class="logo" target="_blank">
+          <img src="./assets/images/logo-iteprevengo.png" width="300" height="24" alt="ItePrevengo">
+        </a>
+
+        <p class="footer-text">
+          &copy; 2023 ItePrevengo. <br> All rights reserved.
+        </p>
+
+        <ul class="social-list">
+
+          <li>
+            <a href="https://www.linkedin.com/company/iteprevengo/" class="social-link" target="_blank">
+              <ion-icon name="logo-linkedin"></ion-icon>
+            </a>
+          </li>
+
+          <li>
+            <a href="https://www.facebook.com/ItePrevengo" class="social-link" target="_blank">
+              <ion-icon name="logo-facebook"></ion-icon>
+            </a>
+          </li>
+
+          <li>
+            <a href="https://www.iteprevengo.com/" class="social-link" target="_blank">
+              <ion-icon name="logo-dribbble"></ion-icon>
+            </a>
+          </li>
+
+          <li>
+            <a href="https://wa.me/51943776124" class="social-link" target="_blank">
+              <ion-icon name="logo-whatsapp"></ion-icon>
+            </a>
+          </li>
+
+          <li>
+            <a href="mailto:consultas@iteprevengo.com" class="social-link" target="_blank">
+              <ion-icon name="mail-outline"></ion-icon>
+            </a>
+          </li>
+
+        </ul>
+
+      </div>
+
+      <ul class="footer-list">
+
+        <li>
+          <p class="h4 footer-list-title">Hablemos</p>
+        </li>
+
+        <li>
+          <a href="mailto:consultas@iteprevengo.com" class="footer-link">consultas@iteprevengo.com</a>
+        </li>
+
+        <li>
+          <a href="tel:014800947" class="footer-link">
+            (01) 480 0947</a>
+        </li>
+        <li>
+          <a href="tel:+51943776124" class="footer-link">
+            +51 943 776 124</a>
+        </li>
+
+      </ul>
+
+      <ul class="footer-list">
+
+        <li>
+          <p class="h4 footer-list-title">Empresa</p>
+        </li>
+
+        <li>
+          <a href="https://www.iteprevengo.com/quienes-somos/" class="footer-link" target="_blank">Quiénes Somos</a>
+        </li>
+
+        <li>
+          <a href="https://www.iteprevengo.com/terminos-y-condiciones/" class="footer-link" target="_blank">Términos y Condiciones</a>
+        </li>
+
+        <li>
+          <a href="https://www.iteprevengo.com/politicas-de-privacidad/" class="footer-link" target="_blank">Politicas de Privacidad</a>
+        </li>
+      </ul>
+
+      <div class="footer-list">
+
+        <p class="h4 footer-list-title">Propósito</p>
+
+        <p class="footer-text">
+          Nuestro propósito es ayudarte a reducir a cero los accidentes y enfermedades producidas en el trabajo, usando herramientas digitales para sus servicios de capacitación, consultoría, supervisión, monitoreos ocupacionales e implementación de la gestión de Seguridad y Salud en el Trabajo
+        </p>
+
+        
+
+      </div>
+
+    </div>
+  </footer>
+      <!-- 
+          - IMG-Zoom
+        -->
+      <script>
+        const lightbox = document.createElement('div')
+        lightbox.id = 'lightbox'
+        document.body.appendChild(lightbox)
+
+        const images = document.querySelectorAll('.img-info img')
+        images.forEach(image => {
+          image.addEventListener('click', e => {
+            lightbox.classList.add('active')
+            const img = document.createElement('img')
+            img.src = image.src
+            while (lightbox.firstChild) {
+              lightbox.removeChild(lightbox.firstChild)
+            }
+            lightbox.appendChild(img)
+          })
+        })
+
+        lightbox.addEventListener('click', e => {
+          if (e.target !== e.currentTarget) return
+          lightbox.classList.remove('active')
+        })
+      </script>
+  <!-- 
+    - ionicon
+  -->
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+    <!-- 
+    - Swiper
+    -->
+  <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+    <script>
+        const swiper = new Swiper('.swiper', {
+            autoHeight: true,
+            loop: true,
+
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
+
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+
+       
+        });
+    </script>
+
+</body>
+
+</html>
